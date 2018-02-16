2dba009e727b3355506c99f2cdffee9988032f86
Crowd Code <crowdcodev2@gmail.com>
2018-02-16 16:18:39 -0500

    Initial commit

---
+++ service/microservice.js
@@ -0,0 +1,19 @@
+
+
+
+function setName(myName)
+{
+	//#Mark this function as implemented by removing this line.
+       // Hello
+	return {}; 
+}
+
+
+function getADT1(name)
+{
+	//#Mark this function as implemented by removing this line.
+	return {}; 
+}
+module.exports = {
+setName:setName,
+getADT1:getADT1 }
