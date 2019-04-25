08325e6921d4d8f79d05c44d0a660cb97ce4d6d3
Microtasking Programing <crowdcodev2@gmail.com>
2019-04-25 17:00:26 -0400

    update package json

--- routes/endpoints.js
+++ routes/endpoints.js
@@ -10,6 +10,16 @@ router.post('/method1', function(req, res) {
  res.send(service.method1(req.body.param));
   });
 
-
+router.get('/', function(req, res, next) {
+    // res.send('respond with a resource');
+    // And insert something like this instead:
+    res.json([{
+        id: 1,
+        endpoint: "method1"
+    }, {
+        id: 2,
+        endpoint: "method1"
+    }]);
+});
 
 module.exports = router;
