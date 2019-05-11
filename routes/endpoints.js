b7a523a4c635b5d31bb21c78ac54556d57c5f055
Microtasking Microtasking <crowdcodev2@gmail.com>
2019-05-11 12:10:22 -0400

    add files

--- routes/endpoints.js
+++ routes/endpoints.js
@@ -2,5 +2,68 @@ var express = require('express');
 var router = express.Router();
 var service = require('../service/microservice');
 
+router.get('/showNumbers', function(req, res) {
+ res.send(service.showNumbers(req.query.source));
+  });
+
+router.post('/showNumbers', function(req, res) {
+ res.send(service.showNumbers(req.body.source));
+  });
+
+
+router.get('/pop', function(req, res) {
+ res.send(service.pop(req.query.source));
+  });
+
+router.post('/pop', function(req, res) {
+ res.send(service.pop(req.body.source));
+  });
+
+
+router.get('/push', function(req, res) {
+ res.send(service.push(req.query.target,req.query.number));
+  });
+
+router.post('/push', function(req, res) {
+ res.send(service.push(req.body.target,req.body.number));
+  });
+
+
+router.get('/shift', function(req, res) {
+ res.send(service.shift(req.query.source));
+  });
+
+router.post('/shift', function(req, res) {
+ res.send(service.shift(req.body.source));
+  });
+
+
+router.get('/unshift', function(req, res) {
+ res.send(service.unshift(req.query.target,req.query.number));
+  });
+
+router.post('/unshift', function(req, res) {
+ res.send(service.unshift(req.body.target,req.body.number));
+  });
+
+
+router.get('/initialize', function(req, res) {
+ res.send(service.initialize(req.query.target));
+  });
+
+router.post('/initialize', function(req, res) {
+ res.send(service.initialize(req.body.target));
+  });
+
+
+router.get('/sayHello', function(req, res) {
+ res.send(service.sayHello(req.query.name));
+  });
+
+router.post('/sayHello', function(req, res) {
+ res.send(service.sayHello(req.body.name));
+  });
+
+
 
 module.exports = router;
--- service/microservice.js
+++ service/microservice.js
@@ -1,3 +1,59 @@
 
-module.exports =  }
-//Sat May 11 2019 12:08:48 GMT-0400 (EDT)
+
+
+function showNumbers(source)
+{
+	//Implementation code here 
+	return {}; 
+}
+
+
+function pop(source)
+{
+	//Implementation code here 
+	return {}; 
+}
+
+
+function push(target, number)
+{
+	//Implementation code here 
+	return {}; 
+}
+
+
+function shift(source)
+{
+	//Implementation code here 
+	return {}; 
+}
+
+
+function unshift(target, number)
+{
+	//Implementation code here 
+	return {}; 
+}
+
+
+function initialize(target)
+{
+	//Implementation code here 
+	return {}; 
+}
+
+
+function sayHello(name)
+{
+	//Implementation code here 
+	return {}; 
+}
+module.exports = {
+showNumbers:showNumbers,
+pop:pop,
+push:push,
+shift:shift,
+unshift:unshift,
+initialize:initialize,
+sayHello:sayHello }
+//Sat May 11 2019 12:10:22 GMT-0400 (EDT)
