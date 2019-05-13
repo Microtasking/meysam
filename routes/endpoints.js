232ea2839b835d1d4eac1de1e59425d21655cd44
Microtasking Microtasking <crowdcodev2@gmail.com>
2019-05-13 03:28:39 +0000

    add files

--- routes/endpoints.js
+++ routes/endpoints.js
@@ -2,21 +2,21 @@ var express = require('express');
 var router = express.Router();
 var service = require('../service/microservice');
 
-router.get('/sayCiao', function(req, res) {
- res.send(service.sayCiao(req.query.name));
+router.get('/sayGoodby', function(req, res) {
+ res.send(service.sayGoodby(req.query.name));
   });
 
-router.post('/sayCiao', function(req, res) {
- res.send(service.sayCiao(req.body.name));
+router.post('/sayGoodby', function(req, res) {
+ res.send(service.sayGoodby(req.body.name));
   });
 
 
-router.get('/sayHello', function(req, res) {
- res.send(service.sayHello(req.query.name));
+router.get('/sayApresto', function(req, res) {
+ res.send(service.sayApresto(req.query.name));
   });
 
-router.post('/sayHello', function(req, res) {
- res.send(service.sayHello(req.body.name));
+router.post('/sayApresto', function(req, res) {
+ res.send(service.sayApresto(req.body.name));
   });
 
 
--- service/microservice.js
+++ service/microservice.js
@@ -1,19 +1,19 @@
 
 
 
-function sayCiao(name)
+function sayGoodby(name)
 {
 	//Implementation code here 
 	return {}; 
 }
 
 
-function sayHello(name)
+function sayApresto(name)
 {
-	 
+	//Implementation code here 
 	return {}; 
 }
 module.exports = {
-sayCiao:sayCiao,
-sayHello:sayHello }
-//Mon May 13 2019 03:22:22 GMT+0000 (UTC)
+sayGoodby:sayGoodby,
+sayApresto:sayApresto }
+//Mon May 13 2019 03:28:39 GMT+0000 (UTC)
