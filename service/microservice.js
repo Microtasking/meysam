bc69b143be55df4dcb93ba36f951d76f5b6e20de
Microtasking Microtasking <crowdcodev2@gmail.com>
2019-05-13 03:19:23 +0000

    add files

--- service/microservice.js
+++ service/microservice.js
@@ -10,10 +10,10 @@ function sayCiao(name)
 
 function sayHello(name)
 {
-	//Implementation code here 
+	 
 	return {}; 
 }
 module.exports = {
 sayCiao:sayCiao,
 sayHello:sayHello }
-//Mon May 13 2019 03:16:16 GMT+0000 (UTC)
+//Mon May 13 2019 03:19:23 GMT+0000 (UTC)
