987bab19951e1da70989dfcd758482570c5823d2
Microtasking Microtasking <crowdcodev2@gmail.com>
2019-05-17 02:17:05 +0000

    add files

--- routes/endpoints.js
+++ routes/endpoints.js
@@ -2,21 +2,156 @@ var express = require('express');
 var router = express.Router();
 var service = require('../service/microservice');
 
-router.get('/sayHello', function(req, res) {
- res.send(service.sayHello(req.query.name));
+router.get('/addTodo', function(req, res) {
+ res.send(service.addTodo(req.query.todo));
   });
 
-router.post('/sayHello', function(req, res) {
- res.send(service.sayHello(req.body.name));
+router.post('/addTodo', function(req, res) {
+ res.send(service.addTodo(req.body.todo));
   });
 
 
-router.get('/sayGoodbye', function(req, res) {
- res.send(service.sayGoodbye(req.query.name));
+router.get('/updateTodo', function(req, res) {
+ res.send(service.updateTodo(req.query.todo));
   });
 
-router.post('/sayGoodbye', function(req, res) {
- res.send(service.sayGoodbye(req.body.name));
+router.post('/updateTodo', function(req, res) {
+ res.send(service.updateTodo(req.body.todo));
+  });
+
+
+router.get('/deleteTodo', function(req, res) {
+ res.send(service.deleteTodo(req.query.todo));
+  });
+
+router.post('/deleteTodo', function(req, res) {
+ res.send(service.deleteTodo(req.body.todo));
+  });
+
+
+router.get('/SaveObject', function(req, res) {
+ res.send(service.SaveObject(req.query.todo));
+  });
+
+router.post('/SaveObject', function(req, res) {
+ res.send(service.SaveObject(req.body.todo));
+  });
+
+
+router.get('/fetchTodo', function(req, res) {
+ res.send(service.fetchTodo(req.query.id));
+  });
+
+router.post('/fetchTodo', function(req, res) {
+ res.send(service.fetchTodo(req.body.id));
+  });
+
+
+router.get('/FetchObject', function(req, res) {
+ res.send(service.FetchObject(req.query.objectId));
+  });
+
+router.post('/FetchObject', function(req, res) {
+ res.send(service.FetchObject(req.body.objectId));
+  });
+
+
+router.get('/DeleteObject', function(req, res) {
+ res.send(service.DeleteObject(req.query.todo));
+  });
+
+router.post('/DeleteObject', function(req, res) {
+ res.send(service.DeleteObject(req.body.todo));
+  });
+
+
+router.get('/UpdateObject', function(req, res) {
+ res.send(service.UpdateObject(req.query.todo));
+  });
+
+router.post('/UpdateObject', function(req, res) {
+ res.send(service.UpdateObject(req.body.todo));
+  });
+
+
+router.get('/fetchAllTodos', function(req, res) {
+ res.send(service.fetchAllTodos(req.query.userId));
+  });
+
+router.post('/fetchAllTodos', function(req, res) {
+ res.send(service.fetchAllTodos(req.body.userId));
+  });
+
+
+router.get('/FetchAllObjects', function(req, res) {
+ res.send(service.FetchAllObjects(req.query.userId));
+  });
+
+router.post('/FetchAllObjects', function(req, res) {
+ res.send(service.FetchAllObjects(req.body.userId));
+  });
+
+
+router.get('/createGroup', function(req, res) {
+ res.send(service.createGroup(req.query.todoArray,req.query.groupId));
+  });
+
+router.post('/createGroup', function(req, res) {
+ res.send(service.createGroup(req.body.todoArray,req.body.groupId));
+  });
+
+
+router.get('/getAllTodoOfaGroup', function(req, res) {
+ res.send(service.getAllTodoOfaGroup(req.query.groupId,req.query.userId));
+  });
+
+router.post('/getAllTodoOfaGroup', function(req, res) {
+ res.send(service.getAllTodoOfaGroup(req.body.groupId,req.body.userId));
+  });
+
+
+router.get('/updatePriorityOfaTodo', function(req, res) {
+ res.send(service.updatePriorityOfaTodo(req.query.id,req.query.priority));
+  });
+
+router.post('/updatePriorityOfaTodo', function(req, res) {
+ res.send(service.updatePriorityOfaTodo(req.body.id,req.body.priority));
+  });
+
+
+router.get('/fetchTodosBasedOnStatus', function(req, res) {
+ res.send(service.fetchTodosBasedOnStatus(req.query.userId));
+  });
+
+router.post('/fetchTodosBasedOnStatus', function(req, res) {
+ res.send(service.fetchTodosBasedOnStatus(req.body.userId));
+  });
+
+
+router.get('/markTodoAsDone', function(req, res) {
+ res.send(service.markTodoAsDone(req.query.id));
+  });
+
+router.post('/markTodoAsDone', function(req, res) {
+ res.send(service.markTodoAsDone(req.body.id));
+  });
+
+
+router.get('/remindOnDueDate', function(req, res) {
+ res.send(service.remindOnDueDate(req.query.userId,req.query.dueDate));
+  });
+
+router.post('/remindOnDueDate', function(req, res) {
+ res.send(service.remindOnDueDate(req.body.userId,req.body.dueDate));
+  });
+
+
+router.get('/markTodoAsArchived', function(req, res) {
+ res.send(service.markTodoAsArchived(req.query.id));
+  });
+
+router.post('/markTodoAsArchived', function(req, res) {
+ res.send(service.markTodoAsArchived(req.body.id));
   });
 
 
--- service/microservice.js
+++ service/microservice.js
@@ -1,19 +1,139 @@
 
 
 
-function sayHello(name)
+function addTodo(todo)
 {
 	//Implementation code here 
 	return {}; 
 }
 
 
-function sayGoodbye(name)
+function updateTodo(todo)
+{
+	//Implementation code here 
+	return {}; 
+}
+
+
+function deleteTodo(todo)
+{
+	//Implementation code here 
+	return {}; 
+}
+
+
+function SaveObject(todo)
+{
+	//Implementation code here 
+	return {}; 
+}
+
+
+function fetchTodo(id)
+{
+	//Implementation code here 
+	return {}; 
+}
+
+
+function FetchObject(objectId)
+{
+	//Implementation code here 
+	return {}; 
+}
+
+
+function DeleteObject(todo)
+{
+	//Implementation code here 
+	return {}; 
+}
+
+
+function UpdateObject(todo)
+{
+	//Implementation code here 
+	return {}; 
+}
+
+
+function fetchAllTodos(userId)
+{
+	//Implementation code here 
+	return {}; 
+}
+
+
+function FetchAllObjects(userId)
+{
+	//Implementation code here 
+	return {}; 
+}
+
+
+function createGroup(todoArray, groupId)
+{
+	//Implementation code here 
+	return {}; 
+}
+
+
+function getAllTodoOfaGroup(groupId, userId)
+{
+	//Implementation code here 
+	return {}; 
+}
+
+
+function updatePriorityOfaTodo(id, priority)
+{
+	//Implementation code here 
+	return {}; 
+}
+
+
+function fetchTodosBasedOnStatus(userId)
+{
+	//Implementation code here 
+	return {}; 
+}
+
+
+function markTodoAsDone(id)
+{
+	//Implementation code here 
+	return {}; 
+}
+
+
+function remindOnDueDate(userId, dueDate)
+{
+	//Implementation code here 
+	return {}; 
+}
+
+
+function markTodoAsArchived(id)
 {
 	//Implementation code here 
 	return {}; 
 }
 module.exports = {
-sayHello:sayHello,
-sayGoodbye:sayGoodbye }
-//Mon May 13 2019 07:13:44 GMT+0000 (UTC)
+addTodo:addTodo,
+updateTodo:updateTodo,
+deleteTodo:deleteTodo,
+SaveObject:SaveObject,
+fetchTodo:fetchTodo,
+FetchObject:FetchObject,
+DeleteObject:DeleteObject,
+UpdateObject:UpdateObject,
+fetchAllTodos:fetchAllTodos,
+FetchAllObjects:FetchAllObjects,
+createGroup:createGroup,
+getAllTodoOfaGroup:getAllTodoOfaGroup,
+updatePriorityOfaTodo:updatePriorityOfaTodo,
+fetchTodosBasedOnStatus:fetchTodosBasedOnStatus,
+markTodoAsDone:markTodoAsDone,
+remindOnDueDate:remindOnDueDate,
+markTodoAsArchived:markTodoAsArchived }
+//Fri May 17 2019 02:17:05 GMT+0000 (UTC)
